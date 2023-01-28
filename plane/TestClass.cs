b926ec3d1c933f23e462a51b48a2d782a1841aa8
chair <projectaa76@gmail.com>
2023-01-28 16:56:07 -0500

    add windowing and basic window test

--- plane/TestClass.cs
+++ plane/TestClass.cs
@@ -1,9 +1,32 @@
-﻿namespace plane;
+﻿using System.Numerics;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+using Silk.NET.Windowing.Sdl;
+
+namespace plane;
 
 public class TestClass
 {
+    private static IWindow? TestWindow;
+
     public static void Run()
     {
-        Console.WriteLine("Hello World");
+        SdlWindowing.Use();
+
+        WindowOptions options = WindowOptions.Default;
+
+        options.Size = new Vector2(1280, 720).ToGeneric().As<int>();
+        options.Title = "Test Window";
+
+        TestWindow = Window.Create(options);
+
+        TestWindow.Load += WindowLoad;
+
+        TestWindow.Run();
+    }
+
+    private static void WindowLoad()
+    {
+        TestWindow!.Center();
     }
 }
