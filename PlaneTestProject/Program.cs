ef990993122bcd133172874734e75d286f9a0ac2
chair <projectaa76@gmail.com>
2023-01-28 16:00:05 -0500

    add editorconfig, change some modifiers

--- PlaneTestProject/Program.cs
+++ PlaneTestProject/Program.cs
@@ -2,9 +2,9 @@
 
 namespace PlaneTestProject;
 
-internal class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         TestClass.Run();
     }
