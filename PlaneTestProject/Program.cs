9bb26d78591f94d15dc99ea03a3e8a02457d6451
chair <87276335+CEO-Chair@users.noreply.github.com>
2023-02-02 08:34:24 -0500

    update PlaneTestProject window title
    hi

--- PlaneTestProject/Program.cs
+++ PlaneTestProject/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        using TestPlaneGame game = new TestPlaneGame("Epic Window");
+        using TestPlaneGame game = new TestPlaneGame("uuuaaasUuuuuGGGooohhhhhhHHH");
 
         game.Run();
     }
