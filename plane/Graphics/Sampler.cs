711647de0e3b4c4a48d64e89527b004655d60ec9
chair <projectaa76@gmail.com>
2023-02-09 23:14:10 -0500

    fix hanging newline

--- plane/Graphics/Sampler.cs
+++ plane/Graphics/Sampler.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Silk.NET.Core.Native;
+﻿using Silk.NET.Core.Native;
 using Silk.NET.Direct3D11;
 
 namespace plane.Graphics;
+
 public class Sampler : IDisposable
 {
     private readonly Renderer Renderer;
