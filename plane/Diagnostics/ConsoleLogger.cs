ce6e504f1d75aa0ab6c01f6118421a4b0f91e1f6
chair <projectaa76@gmail.com>
2023-02-13 21:36:35 -0500

    fix some threading stuff in ConsoleLogger

--- plane/Diagnostics/ConsoleLogger.cs
+++ plane/Diagnostics/ConsoleLogger.cs
@@ -8,25 +8,30 @@ using Silk.NET.Assimp;
 namespace plane.Diagnostics;
 public class ConsoleLogger : Logger
 {
+    private object ConsoleLock = new object();
+
     public override void WriteLine(string? message, LogSeverity severity, DateTime time)
     {
         message ??= "[Empty Log]";
 
-        switch (severity)
+        lock (ConsoleLock)
         {
-            case LogSeverity.Info:
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                break;
-            case LogSeverity.Warning:
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                break;
-            case LogSeverity.Error:
-                Console.ForegroundColor = ConsoleColor.Red;
-                break;
+            switch (severity)
+            {
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.WriteLine($"[{time:mm:ss:ffff}] {message}");
+
+            Console.ResetColor();
         }
-
-        Console.WriteLine($"[{time:mm:ss:ffff}] {message}");
-
-        Console.ResetColor();
     }
 }
